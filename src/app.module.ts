dc943353930f51e45e710c9e6ca0028d74ea564a
Luixinho <ticolucas7@gmail.com>
2021-09-14 17:54:06 -0300

    procfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Admin:admin@cluster0.nlo7n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
